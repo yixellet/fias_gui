f0de146933d8e1edff3e766f23c01e92d8b53a52
Kirill Kotelevsky <yixwellet@gmail.com>
2021-02-23 17:19:32 +0400

    Выполнена верстка сайдбара без логики: 1. Добавлена форма выбора отображаемых элементов 2. Добавлена форма фильтрации вывода

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,16 @@
 import Header from '../Header/Header';
+import Sidebar from '../Sidebar/Sidebar';
+
+import styles from './App.module.css';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <main className={styles.container}>
+        <Sidebar />
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Sidebar/ViewForm/Input/Input.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Input.module.css';
+
+class Input extends React.Component {
+  render() {
+    return (
+        <div>
+          <input className={styles.input} 
+          type="radio" id={this.props.id} 
+          name={this.props.id} 
+          value={this.props.id} />
+          <label className={styles.label} 
+          htmlFor={this.props.id}>{this.props.name}</label>
+        </div>
+    );
+  }
+}
+
+export default Input;

587c7984400fa5ef796609a5e260cd463678ff3b
yixellet <yixwellet@gmail.com>
2022-01-30 21:18:18 +0400

    начал делать карту

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import leaflet from 'leaflet';
+import styles from './Map.module.css';
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      map: null
+    }
+    this.createMap = this.createMap.bind(this)
+  }
+
+  createMap() {
+    const opts = this.props.gauges !== null ? {center: [47.2,47.2], zoom: 7} : {center: [this.props.lon, this.props.lat], zoom: 10}
+    return leaflet.map('map',opts)
+  }
+
+  componentDidMount() {
+    const m = this.createMap()
+    this.setState({map: m})
+    leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(m);
+  }
+
+  render() {
+    return (
+      <div id='map' className={styles.map}></div>
+    );
+  }
+}
+
+export default Map;
